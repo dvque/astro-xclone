9dac8e848ec8a94b4ff4f461a07e459cfcf1b7be
dvque <dvdquerol@gmail.com>
2023-12-30 15:52:39 +0100

    Remove unused dependencies and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), solidJs()],
+  integrations: [tailwind(), solidJs()],
   output: 'server',
   adapter: vercel()
 });
