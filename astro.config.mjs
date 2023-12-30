328a6ff1f1d49a58aa325035ce761ec6ac09be7f
dvque <dvdquerol@gmail.com>
2023-12-30 15:46:42 +0100

    added vercel and solidJs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server'
+  integrations: [tailwind(), react(), solidJs()],
+  output: 'server',
+  adapter: vercel()
 });
