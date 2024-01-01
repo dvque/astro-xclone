983fd1ae4bfb86cad9bb05a82dd25f940bf0d36c
dvque <dvdquerol@gmail.com>
2024-01-01 17:55:40 +0100

    Authbuttons with session rather than by cookies

--- src/components/AuthButtons.tsx
+++ src/components/AuthButtons.tsx
@@ -1,15 +1,18 @@
-import { Show } from 'solid-js';
+import { Show} from 'solid-js';
+import type { Session } from '@supabase/supabase-js';
 
+export default function AuthButtons({session}: {session: Session | null}) {
 
-export default function AuthButtons({ accessToken, refreshToken }) {
+    console.log("session: ", session);
+
     return (
         <>
-            <Show when={accessToken && refreshToken}>
+            <Show when={session != null}>
                 <li>
                     <a href="/dashboard" class="block text-zinc-900 dark:text-gray-200">Dashboard</a>
                 </li>
             </Show>
-            <Show when={!accessToken && !refreshToken}>
+            <Show when={session == null}>
                 <li>
                     <a href="/register" class="block text-zinc-900 dark:text-gray-200">Register</a>
                 </li>
