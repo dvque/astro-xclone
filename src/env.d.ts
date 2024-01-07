27702eb6e00adbb5e666ee39220b6fbcaeb6bee1
dvque <dvdquerol@gmail.com>
2024-01-07 15:05:48 +0100

    Add types for Post and User entities

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
+import { type Database } from './types/database'
+
+type PostEntity = Database['public']['Tables']['posts']['Row']
+type UserEntity = Database['public']['Tables']['users']['Row']
+
+export type Post = PostEntity & {
+  users: UserEntity
+}
+
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
