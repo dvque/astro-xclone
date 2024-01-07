e46817bd6dcd0412728458258d934718a95dc493
dvque <dvdquerol@gmail.com>
2024-01-07 15:06:58 +0100

    Add local storage persistence for board state

--- src/scripts/board.ts
+++ src/scripts/board.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'solid-js/store'
+import { createEffect } from 'solid-js'
 import { type Database } from '../types/database'
 
 type Posts = Database['public']['Tables']['posts']['Row']
@@ -6,4 +7,9 @@ type Posts = Database['public']['Tables']['posts']['Row']
 const LOCAL_STORAGE_KEY = 'cart'
 const stored: string | null = localStorage.getItem(LOCAL_STORAGE_KEY)
 const parsed: Posts[] = (stored != null) ? JSON.parse(stored) : []
+
 export const [board, setBoard] = createStore<Posts[]>(parsed ?? [])
+
+createEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(board))
+})
